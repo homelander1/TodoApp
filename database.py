724b65bbaf6b1ded38dd7b86b5c0c0f2483c5e97
Roman Zhuk <oneai0558@gmail.com>
2025-06-15 20:13:29 +0300

    API + DB + tests

--- database.py
+++ database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  # for sqlite
-SQLALCHEMY_DATABASE_URL = 'postgresql://boy:work@localhost:5432/todoAppDB'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  # for sqlite
+# SQLALCHEMY_DATABASE_URL = 'postgresql://boy:work@localhost:5432/todoAppDB'
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) # for sqlite
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) # for sqlite
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
